846000a90ae75bc2b0f61c127419d834909a7388
Alessandro Corbelli <alessandro.corbelli@guest.it>
2023-11-11 15:06:17 +0100

    fix

--- models/project_project_stage.py
+++ models/project_project_stage.py
@@ -4,7 +4,7 @@ from odoo import fields, models
 
 
 class ProjectStage(models.Model):
-    _inherit = "project.stage"
+    _inherit = "project.project.stage"
 
     column_limits = fields.Integer(string="Column limits")
     strict_column_limits = fields.Boolean(string="Strict column limits", default=False)
